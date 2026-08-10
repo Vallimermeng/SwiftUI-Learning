8fae72ee33a6fdbcd24397f03348082c6706390a
Petros Meliksetyan <turbo.petya77@gmail.com>
2026-08-10 23:34:48 +0700

    Add SwiftUI learning projects

---
+++ Guess the flag/Guess the flag/ContentView.swift
@@ -0,0 +1,100 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showinScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    
+    
+    @State private var attempt = 0
+    
+    var body: some View {
+        ZStack{
+            
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.3, green: 0.5, blue: 0.5), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.45, blue: 0.46), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 400)
+            
+            VStack{
+                Spacer()
+                Text("Угадай флаг").font(.largeTitle.weight(.bold)).foregroundStyle(.white)
+                Spacer()
+                Spacer()
+                Text("Счет: \(score)").foregroundStyle(.white).font(.title.bold())
+                if(averageRate().isNaN){
+                    Text("Рейтинг: 0%").foregroundStyle(.white).font(.title.bold())
+                }else{
+                    Text("Рейтинг: \(averageRate())%        ").foregroundStyle(.white).font(.title.bold())
+                }
+                
+                Spacer()
+                
+                VStack(spacing: 15){
+                    VStack{
+                        Text("Нажмите на флаг").foregroundStyle(.secondary).font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer]).font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3){number in
+                        Button{
+                            flagTapped(number)
+                        }label:{
+                            Image(countries[number]).clipShape(.capsule).shadow(radius: 5)
+                        }
+                    }
+                }
+                //Vstack 15
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+            }
+            .padding()
+        }
+        //ZStack
+        .ignoresSafeArea()
+        .alert(scoreTitle, isPresented: $showinScore){ // ?
+            Button("Продолжить", action: askQuestion)
+        }message: {
+            Text("Ваш счет: \(score). Оценка: \(averageRate()) %")
+            
+        }
+        
+    }
+    
+    func flagTapped(_ number: Int){
+        if number == correctAnswer{
+            scoreTitle = "Верно"
+            score+=1
+            attempt+=1
+        }else{
+            scoreTitle = "Неверно, это флаг \(countries[number])"
+            attempt+=1
+        }
+        showinScore = true
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func averageRate() -> Double{
+        
+        let score_tmp = Double(score)
+        let attemp_tmp = Double(attempt)
+        let result = (score_tmp /  (attemp_tmp)) * 100
+        return result
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guess the flag/Guess the flag/Guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_LearnApp.swift
+//  iOS Learn
+//
+//  Created by Петя on 09.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
