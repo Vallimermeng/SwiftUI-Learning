9970376e45117c4521fc6a400c222b3cfce73051
Petros Meliksetyan <turbo.petya77@gmail.com>
2026-08-12 22:26:03 +0700

    fix: added right format for percentage

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Петя on 12.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
